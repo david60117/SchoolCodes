5da7bc3a8a8b6d6a72e767d68d583a0210acdc89
david60117 <davidjhernandezm4@gmail.com>
2025-09-18 08:40:01 -0600

    Actu.

--- Parcial 2/Arreglos/Arreglo2.java
+++ Parcial 2/Arreglos/Arreglo2.java
@@ -1,10 +1,11 @@
 public class Arreglo2 {
     public static void main(String[] args) {
-        String nombres[]= new String[4];
+        String nombres []={"David ","Jonathan ","Hernández ","Mejía "};
+        /*String nombres[]= new String[4];
         nombres[0]="David ";
         nombres[1]="Jonathan ";
         nombres[2]="Hernández ";
-        nombres[3]="Mejía ";
+        nombres[3]="Mejía ";*/
         for(String i : nombres){
             System.out.print(i);
         }
