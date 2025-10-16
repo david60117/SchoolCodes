fac9602bc4a9faa5a957525ac760200b508b9303
david60117 <davidjhernandezm4@gmail.com>
2025-10-16 15:29:36 -0600

    Actualización

--- Parcial 2/Arreglos/OperacionesAd.java
+++ Parcial 2/Arreglos/OperacionesAd.java
@@ -167,13 +167,14 @@ public class OperacionesAd {
             return;
         }
         for (int i = 0; i < arreglo.length - 1; i++) {
-            for (int j = 0; j < arreglo.length - 1 - i; j++) {
-                if (arreglo[j] > arreglo[j + 1]) {
-                    int temp = arreglo[j];
-                    arreglo[j] = arreglo[j + 1];
-                    arreglo[j + 1] = temp;
+
+                for (int j = 0; j < arreglo.length - 1 - i; j++) {
+                    if (arreglo[j] > arreglo[j + 1]) {
+                        int temp = arreglo[j];
+                        arreglo[j] = arreglo[j + 1];
+                        arreglo[j + 1] = temp;
+                    }
                 }
-            }
         }
         System.out.println("Arreglo ordenado en forma ascendente.");
     }
---
+++ Parcial 3/Matrices/Matriz11.java
@@ -0,0 +1,260 @@
+import java.util.Scanner;
+
+public class Matriz11 {
+    int x;
+    int y;
+    int [][] arreglo;
+
+    public Matriz11(){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Numeró deseado de filas que contenga la matriz: ");
+        x=sc.nextInt();
+        System.out.print("Número deseado de columnas que contenga la matriz: ");
+        y=sc.nextInt();
+        arreglo = new int [x][y];
+    }
+
+   public void almacenar() {
+        for (int f = 0; f < x; f++) {
+            for(int c = 0; c < y; c++){
+                while (true) {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Elemento [" + f + "]["+ c +"]: ");
+                if (sc.hasNextInt()) {
+                    arreglo[f][c] = sc.nextInt();
+                    break; 
+                } else {
+                    System.out.println("Mensaje: Entrada inválida para el elemento. Ingrese un entero.");
+                    sc.next(); 
+                }
+            }
+            }
+        }
+    } 
+
+    public void mostrar() {
+        if (arreglo.length == 0) {
+            System.out.println("La matriz está vacía.");
+            return;
+        }
+        System.out.println("Recorrido ascendente:");
+        for (int f = 0; f < x; f++) {
+            System.out.println();
+            for(int c = 0; c < y; c++){
+                System.out.print(arreglo[f][c] + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public void buscar(){
+        Scanner sc = new Scanner(System.in);
+        if (arreglo.length == 0) {
+            System.out.println("La matriz está vacía.");
+            return;
+        }
+        int valor;
+        while (true) {
+            System.out.print("Ingrese el valor a modificar: ");
+
+            if (sc.hasNextInt()) {
+                valor = sc.nextInt(); 
+                break;                
+            } else {
+                System.out.println("Mensaje: Entrada inválida. Por favor, ingrese un número entero.");
+                sc.next();            
+            }
+        }
+        boolean encontrado=false;
+        
+        int fila=0;
+        int columna=0;
+        for (int f = 0; f < x; f++) {
+            for(int c = 0; c < y; c++){
+                if (valor==arreglo[f][c]) {
+                    encontrado=true;
+                    fila=f;
+                    columna=c;
+                    break;
+                }
+            }
+        }
+        if (encontrado){
+            System.out.println("El valor " + valor + " esta en la posición ["+fila+"]["+columna+"]" );
+        } else {
+            System.out.println("Es valor "+ valor + " no esta en el arreglo");
+        }
+    }
+
+    public void modificar(){
+        Scanner sc = new Scanner(System.in);
+        if (arreglo.length == 0) {
+            System.out.println("La matriz está vacía.");
+            return;
+        }
+        int valor;
+        while (true) {
+            System.out.print("Ingrese el valor a buscar: ");
+
+            if (sc.hasNextInt()) {
+                valor = sc.nextInt(); 
+                break;                
+            } else {
+                System.out.println("Mensaje: Entrada inválida. Por favor, ingrese un número entero.");
+                sc.next();            
+            }
+        }
+        boolean encontrado=false;
+        
+        int fila=0;
+        int columna=0;
+        for (int f = 0; f < x; f++) {
+            for(int c = 0; c < y; c++){
+                if (valor==arreglo[f][c]) {
+                    encontrado=true;
+                    fila=f;
+                    columna=c;
+                    break;
+                }
+            }
+        }
+        if (encontrado){
+            while (true) {
+                System.out.print("Valor a modificar del elemento [" + fila + "]["+ columna +"]: ");
+                if (sc.hasNextInt()) {
+                    arreglo[fila][columna] = sc.nextInt();
+                    break; 
+                } else {
+                    System.out.println("Mensaje: Entrada inválida para el elemento. Ingrese un entero.");
+                    sc.next(); 
+                }
+            }
+        } else {
+            System.out.println("Es valor "+ valor + " no esta en el arreglo");
+        }
+    }
+
+    public void eliminarelemento(){
+        Scanner sc = new Scanner(System.in);
+        if (arreglo.length == 0) {
+            System.out.println("La matriz está vacía.");
+            return;
+        }
+        int valor;
+        while (true) {
+            System.out.print("Ingrese el valor a buscar: ");
+
+            if (sc.hasNextInt()) {
+                valor = sc.nextInt(); 
+                break;                
+            } else {
+                System.out.println("Mensaje: Entrada inválida. Por favor, ingrese un número entero.");
+                sc.next();            
+            }
+        }
+        boolean encontrado=false;
+        
+        int fila=0;
+        int columna=0;
+        for (int f = 0; f < x; f++) {
+            for(int c = 0; c < y; c++){
+                if (valor==arreglo[f][c]) {
+                    encontrado=true;
+                    fila=f;
+                    columna=c;
+                    break;
+                }
+            }
+        }
+        if (encontrado){
+            System.out.println("Elemento eliminado");
+            arreglo[fila][columna] = 0;
+        } else {
+            System.out.println("Es valor "+ valor + " no esta en el arreglo");
+        }
+    }
+
+    public void eliminarConjunto() {
+        arreglo = new int[0][0];
+        System.out.println("Todos los elementos fueron eliminados.");
+    }
+    
+    public void ordenarAsc() {
+        if (x == 0 || y == 0) {
+            System.out.println("La matriz está vacía. No se puede ordenar.");
+            return;
+        }
+
+        int totalElementos = x * y;
+        int[] arregloPlano = new int[totalElementos];
+        int indice = 0;
+
+        for (int f = 0; f < x; f++) {
+            for (int c = 0; c < y; c++) {
+                arregloPlano[indice] = arreglo[f][c];
+                indice++;
+            }
+        }
+
+        for (int i = 0; i < totalElementos - 1; i++) {
+            for (int j = i + 1; j < totalElementos; j++) {
+                if (arregloPlano[i] > arregloPlano[j]) {
+                    int temp = arregloPlano[i];
+                    arregloPlano[i] = arregloPlano[j];
+                    arregloPlano[j] = temp;
+                }
+            }
+        }
+
+        indice = 0; 
+        for (int f = 0; f < x; f++) {
+            for (int c = 0; c < y; c++) {
+                arreglo[f][c] = arregloPlano[indice];
+                indice++;
+            }
+        }
+        System.out.println("La matriz ha sido ordenada en forma ascendente.");
+    }
+
+    public static void main(String[] args) {
+        Matriz11 operacion = new Matriz11();
+        int op=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Operaciones de arreglos");
+
+        do {
+            System.out.println("\nMENÚ PRINCIPAL");
+            System.out.println("1. Almacenar elementos");
+            System.out.println("2. Mostrar matriz");
+            System.out.println("3. Buscar");
+            System.out.println("4. Modificar");
+            System.out.println("5. Eliminar elemento");
+            System.out.println("6. Eliminar matriz");
+            System.out.println("7. Ordenar");
+            System.out.println("0. Salir");
+            System.out.print("Seleccione una opción: ");
+
+            if (sc.hasNextInt()) {
+                op = sc.nextInt();
+            } else {
+                op = -1;
+                sc.next(); 
+            }
+            
+            switch (op) {
+                case 1 -> operacion.almacenar();
+                case 2 -> operacion.mostrar();
+                case 3 -> operacion.buscar();
+                case 4 -> operacion.modificar();
+                case 5 -> operacion.eliminarelemento();
+                case 6 -> operacion.eliminarConjunto();
+                case 7 -> operacion.ordenarAsc();
+                case 0 -> System.out.println("Saliendo del programa...");
+                default -> System.out.println("Opción inválida");
+            }
+        } while (op != 0);
+    }
+
+
+
+}
